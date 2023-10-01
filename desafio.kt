e6f0629e718176bad0850d916c2752f4c4f6c8a6
Gian <giansantos.dev@gmail.com>
2023-10-01 20:41:10 -0300

    Resolução do desafio
    Foi adicionado o parâmetro de nome para a classe Usuário
    Foi adicionado o nível da formação e feita uma alteração na função matricular para fazer com que seja possível cadastrar mais de um usuário na formação
    Foram feitos os testes com conteúdos de uma formação de desenvolvimento web

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,60 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel {
+    BASICO,
+    INTERMEDIARIO,
+    AVANCADO
+}
 
-class Usuario
+data class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracaoEmHoras: Int)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+data class Formacao(val nome: String, nivel: Nivel, val conteudos: List<ConteudoEducacional>) {
     val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
-    }
+
+    fun matricular(vararg usuarios: Usuario) {
+        inscritos.addAll(usuarios)
+        for (usuario in usuarios) {
+            println("Usuário ${usuario.nome} cadastrado com sucesso!")
+        }
+
+    }   
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val gian = Usuario("Gian Santos")
+    val aecio = Usuario("Aécio José")
+    val guilherme = Usuario("Guilherme Lucena")
+    val joao = Usuario("João Silva")
+    
+
+    val introWeb = ConteudoEducacional("Introdução à web", 20)
+    val logicaDeProgramacao = ConteudoEducacional("Lógica de programação", 60)
+    val htmlECss = ConteudoEducacional("HTML e CSS", 60)
+    val javascript = ConteudoEducacional("Javascript", 80)
+    val desenvolvimentoDeInterfaces = ConteudoEducacional("Desenvolvimento de interfaces", 60)
+    val linguagensBackend = ConteudoEducacional("Linguagens back-end", 60)
+    val bancoDeDados = ConteudoEducacional("Bancos de dados", 60)
+    val desenvolvimentoDeAPIs = ConteudoEducacional("Desenvolvimento de APIs", 60)
+    val projeto1 = ConteudoEducacional("Desenvolvimento de um website simples", 40)
+    val projeto2 = ConteudoEducacional("Desenvolvimento de um aplicativo web", 80)
+    val mercadoDeTrabalho = ConteudoEducacional("Mercado de trabalho", 20)
+
+    val conteudos = listOf(
+        introWeb,
+        logicaDeProgramacao,
+        htmlECss,
+        javascript,
+        desenvolvimentoDeInterfaces,
+        linguagensBackend,
+        bancoDeDados,
+        desenvolvimentoDeAPIs,
+        projeto1,
+        projeto2,
+        mercadoDeTrabalho
+    )
+
+    val formacao = Formacao("Desenvolvimento Web", Nivel.INTERMEDIARIO, conteudos)
+    formacao.matricular(gian, aecio, guilherme, joao)
 }
